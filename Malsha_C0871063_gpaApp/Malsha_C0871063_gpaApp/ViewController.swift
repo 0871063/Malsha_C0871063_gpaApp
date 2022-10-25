aba71cbf53803bcc0c91ecf5235a6ddad40ec95f
0871063 <c0871063@mylambton.ca>
2022-10-25 17:54:42 -0400

    Initialize

---
+++ Malsha_C0871063_gpaApp/Malsha_C0871063_gpaApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Malsha_C0871063_gpaApp
+//
+//  Created by Malsha Lambton on 2022-10-25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
